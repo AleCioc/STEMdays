90a98af43bb7258c60ee0c7f7379c5da1a97a099
alecioc <alecioc@AleCiocM1.homenet.telecomitalia.it>
2021-06-24 15:26:52 +0200

    add youtube video search from songs in dataset

--- alecioc/dashboard_example.py
+++ alecioc/dashboard_example.py
@@ -2,6 +2,7 @@ import os
 import streamlit as st
 from PIL import Image
 import pandas as pd
+from youtubesearchpython import VideosSearch
 
 
 st.set_page_config(
@@ -19,6 +20,16 @@ sidebar_page = st.sidebar.selectbox(
     ["Home", "Pagina 1", "Pagina 2"]
 )
 
+# TODO MAYBE -> simple caching
+df = pd.read_csv(os.path.join("data", "10k_random_tracks.csv"))
+
+# Questo serve per convertire le colonne che dovrebbero essere di tipo lista ma vengono lette come stringhe dal csv
+# Magari wrappiamo alcune di queste robe e gliele diamo
+from ast import literal_eval
+df.artists = df.artists.apply(literal_eval)
+
+# Simple menu
+
 if sidebar_page == "Home":
 
     # Modo facile di scrivere testo su streamlit
@@ -26,10 +37,18 @@ if sidebar_page == "Home":
     st.header("Benvenut* in Webby, un progetto per STEM days 2021!")
     st.text("Usa il menu sulla sinistra per navigare fra le possibili schermate.")
 
-    # Modo "difficile" di scrivere testo su streamlit
-    original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">A custom style in HTML+CSS</p>'
+    # Modo "difficile" (HTML+CSS) di scrivere testo su streamlit
+    original_title = """
+        <p style="font-family:Courier; color:Blue; font-size: 20px;">
+            A custom style in HTML+CSS
+        </p>    
+    """
     st.markdown(original_title, unsafe_allow_html=True)
-    new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">Another custom style in HTML+CSS</p>'
+    new_title = """
+        <p style="font-family:sans-serif; color:Green; font-size: 42px;">
+            Another custom style in HTML+CSS
+        </p>
+    """
     st.markdown(new_title, unsafe_allow_html=True)
 
     # Immagine
@@ -39,11 +58,17 @@ elif sidebar_page == "Pagina 1":
     # Tabella
     st.header("Inserire una tabella (dataframe)")
     st.subheader("Qui inseriamo una tabella con 10000 canzoni")
-    df = pd.read_csv(os.path.join("data", "10k_random_tracks.csv"))
     st.dataframe(df)
 
 elif sidebar_page == "Pagina 2":
-    st.header("Inserire una video da youtube")
-    st.subheader("Qui sarebbe fighissimo recuperare i link delle canzoni che abbiamo")
-    st.video("https://www.youtube.com/watch?v=AGZah79LiIE")
+    st.header("Inserire un video da youtube")
+    st.subheader("Qui recuperiamo il link Youtube della prima canzone nel dataset")
+    name_and_artists_string = " ".join([df.iloc[0]["name"]] + [artist for artist in df.iloc[0]["artists"]])
+    st.subheader(name_and_artists_string)
+    videosSearch = VideosSearch(
+        name_and_artists_string,
+        limit=1
+    )
+    st.write(videosSearch.result()["result"][0]["link"])
+    st.video(videosSearch.result()["result"][0]["link"])
 
