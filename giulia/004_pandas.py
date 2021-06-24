67c9cb4e0704cb0d55021e5208a648cdcd7dd0c5
Giulia Milan <giuliapuntoit96@gmail.com>
2021-06-24 22:51:40 +0200

    Add examples

--- giulia/004_pandas.py
+++ giulia/004_pandas.py
@@ -1,1 +1,14 @@
-# dataframe, leggi pezzi di csv?
+# dataframe, leggi pezzi di csv
+
+import pandas as pd
+
+df = pd.read_csv("../alecioc/data/10k_random_tracks.csv", index_col=0)
+
+#print(df.index)
+
+print(df.shape)
+
+print(df[["name", "popularity"]])
+
+print(df.iloc[7])
+
